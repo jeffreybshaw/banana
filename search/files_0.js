1e6e66a96d8c5ac04a7f4228a7a183ac84a04b9e
Jeff Shaw <jeffrey.b.shaw@intel.com>
2015-07-30 11:34:18 -0700

    Add documentation

---
+++ search/files_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['libbanana_2eh',['libbanana.h',['../libbanana_8h.html',1,'']]]
+];
---
+++ search/searchdata.js
@@ -0,0 +1,27 @@
+var indexSectionsWithContent =
+{
+  0: "bl",
+  1: "l",
+  2: "b",
+  3: "b",
+  4: "b"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files",
+  2: "functions",
+  3: "enums",
+  4: "enumvalues"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Files",
+  2: "Functions",
+  3: "Enumerations",
+  4: "Enumerator"
+};
+
