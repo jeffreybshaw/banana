24236742797d0a5d932a9ffd0693e0ae84957468
Jeff Shaw <jeffrey.b.shaw@intel.com>
2022-03-12 21:11:23 +0000

    Remove old site
    Signed-off-by: Jeff Shaw <jeffrey.b.shaw@intel.com>

--- api/search/enums_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['banana_5fcolor',['banana_color',['../libbanana_8h.html#aaca8702d06d756f3491d96da2d87a325',1,'libbanana.h']]],
-  ['banana_5fsize',['banana_size',['../libbanana_8h.html#a5f6321a8145f9a91947691af2115dc41',1,'libbanana.h']]]
-];
--- api/search/files_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['libbanana_2eh',['libbanana.h',['../libbanana_8h.html',1,'']]]
-];
